d907c2e066ee250cdae068e866bb2cfc4a465212
Rayan Morais <rayan_cm2021@icloud.com>
2025-07-14 17:20:01 -0300

    Add watchlist and finish the application

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,27 +1,112 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles.css";
+import MovieCard from "./MovieCard";
 
-export default function MoviesGrid() {
-  const [movies, setMovies] = useState([]);
+export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
+  const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    fetch("movies.json")
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
-  }, []);
+  const [genre, setGenre] = useState("All Genre");
+  const [rating, setRating] = useState("All");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
+  const matchesGenre = (movie, genre) => {
+    return (
+      genre === "All Genre" || movie.genre.toLowerCase() === genre.toLowerCase()
+    );
+  };
+
+  const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const matchesRating = (movie, rating) => {
+    switch (rating) {
+      case "All":
+        return true;
+
+      case "Good":
+        return movie.rating >= 8;
+
+      case "Ok":
+        return movie.rating >= 5 && movie.rating < 8;
+
+      case "Bad":
+        return movie.rating < 5;
+
+      default:
+        return false;
+    }
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      matchesGenre(movie, genre) &&
+      matchesRating(movie, rating) &&
+      matchesSearchTerm(movie, searchTerm)
+  );
 
   return (
-    <div className="movies-grid">
-      {movies.map((movie) => (
-        <div key={movie.id} className="movie-card">
-          <img src={`images/${movie.image}`} alt={movie.title} />
-          <div className="movie-card-info">
-            <h3 className="movie-card-title">{movie.title}</h3>
-            <p className="movie-card-genre">{movie.genre}</p>
-            <p className="movie-card-rating">{movie.rating}</p>
-          </div>
+    <div>
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdwon"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
         </div>
-      ))}
+
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdwon"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard
+            movie={movie}
+            key={movie.id}
+            toggleWatchlist={toggleWatchlist}
+            isWatchlisted={watchlist.includes(movie.id)}
+          ></MovieCard>
+        ))}
+      </div>
     </div>
   );
 }
